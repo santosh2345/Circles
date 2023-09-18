1f1c003aa441e6fea1c651d392aaf35b10bbabb4
santosh <santosh@localhost.localdomain>
2023-09-18 14:54:03 -0400

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import SideBar from './components/sideBar/sideBar';
-import Home from './components/home/home';
-import Router from './route';
+import logo from "./logo.svg";
+import "./App.css";
+import SideBar from "./components/sideBar/sideBar";
+import Home from "./components/home/home";
+import Router from "./route";
 
 function App() {
-  return (
-    <Router />
-    // <div className='flex flex-row w-full justify-between h-screen'>
-    // <SideBar />
-    // <Home />
-
-    // </div>
-  )
+  return <Router />;
 }
 
 export default App;
